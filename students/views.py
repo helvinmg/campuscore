9020469fd0b34bf2b0a791fad4628141bbbd4b54
helvinmg <helvin.m.g@gmail.com>
2026-02-04 10:18:14 +0530

    added pagesapp

--- students/views.py
+++ students/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def student_list(request):
-    return render(request,'student_list.html')
+    return render(request,'students/student_list.html')
     #return HttpResponse("Welcome to the Student Directory")
