671b55d482fd1e4a0c76225064f9c024488ee1da
helvinmg <helvin.m.g@gmail.com>
2026-02-04 12:22:34 +0530

    updated course catalog to be dynamic

--- courses/views.py
+++ courses/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def course_catalog (request):
-    return render(request,'courses/course_catalog.html')
+    courses=['Btech CSE','Btech ECE','Btech ME','Btech CE','Btech EE','Mtech Cyber Security','Mtech Embedded Systems']
+    count={"UG":4,"PG":2}
+    return render(request,'courses/course_catalog.html',{'courses':courses,'count':count})
