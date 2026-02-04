47e1a8d4ce5d82b9c97b5162ec70b920179b5eac
helvinmg <helvin.m.g@gmail.com>
2026-02-04 11:42:49 +0530

    added DTL on students app

--- courses/views.py
+++ courses/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def course_catalog (request):
-    return HttpResponse('Browse our available courses here')
+    return render(request,'courses/course_catalog.html')
--- students/views.py
+++ students/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def student_list(request):
-    return render(request,'students/student_list.html')
-    #return HttpResponse("Welcome to the Student Directory")
+    #data fetching data from db
+    stud_records=[{'slno':1,'name':'rahul','course':'cs','sem':3},{'slno':2,'name':'reena','course':'cs','sem':3}]
+    data={'admission_closed':False,"count":59,'students':stud_records}
+    return render(request,'students/student_list.html',{'adm_data':data})
+    #render can accept a 3rd argument in form of dictionary - context
+   
